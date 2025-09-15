77e9a4e96cd42beea47839e518486053a46af9b0
H. William Connors II <hwconnors@gmail.com>
2025-09-15 14:23:37 -0400

    Allow support of enum-class or enum-values

--- src/main/java/com/github/hwc2243/servicebuilder/model/Attribute.java
+++ src/main/java/com/github/hwc2243/servicebuilder/model/Attribute.java
@@ -1,5 +1,12 @@
 package com.github.hwc2243.servicebuilder.model;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
@@ -8,6 +15,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Attribute {
+	protected static Logger logger = LoggerFactory.getLogger(Attribute.class);
+
 	@Getter
 	@Setter
 	protected String name = null;
@@ -25,4 +34,18 @@ public class Attribute {
 	@Setter
 	@JacksonXmlProperty(localName = "enum-class")
 	protected String enumClass = null;
+	
+	@Getter
+	@Setter
+	protected List<String> enumValues = null;
+	
+	@JacksonXmlProperty(isAttribute = true, localName = "enum-values")
+    public void setEnumValues (String enumValues) {
+        if (enumValues != null && !enumValues.isEmpty()) {
+            this.enumValues = Arrays.stream(enumValues.split("\\s+"))
+                                    .map(String::trim)
+                                    .collect(Collectors.toList());
+            logger.info("Set enum values: " + this.enumValues);
+        }
+    }
 }
